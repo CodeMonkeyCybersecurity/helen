a33909b76e31db2f2ce5b79fd62128c219be6e0f
codeMonkeyCybersecurity <git@cybermonkey.net.au>
2025-06-04 05:09:08 +0800

    Create install-hugo.sh

---
+++ install-hugo.sh
@@ -0,0 +1,10 @@
+# Find latest version at https://github.com/gohugoio/hugo/releases/latest
+HUGO_VER=0.127.0  # <-- set to latest (or at least 0.128.0, e.g. 0.127.0 is below the requirement)
+# Use 0.128.0 or newer! Example:
+HUGO_VER=0.128.2
+
+cd /tmp
+wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VER}/hugo_extended_${HUGO_VER}_linux-amd64.tar.gz
+tar -xvf hugo_extended_${HUGO_VER}_linux-amd64.tar.gz
+sudo mv hugo /usr/local/bin/
+hugo version
